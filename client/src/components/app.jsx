bf0a8ed61e46ad041c5ad5b34033de68a24e5f88
BrandonALittle <brandon.a.little@gmail.com>
2018-03-17 16:51:05 -0700

    updated app.jsx with some apollo comments, added problem and problemsView components

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Landing from './landing.jsx'
+import NavBar from './navBar.jsx'
 
 class App extends React.Component {
   render () {
     return (
       <div>
+        <NavBar />
         <Route exact path='/' component={ Landing } />
       </div>
     )
   }
 }
 
-module.exports = App;
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, browserHistory } from 'react-router-dom';
 import App from './components/app.jsx';
+// import { ApolloProvider } from 'react-apollo'
+// import { ApolloClient } from 'apollo-client'
+// import { HttpLink } from 'apollo-link-http'
+// import { InMemoryCache } from 'apollo-cache-inmemory'
+
+
+// APOLLO SERVER CONNECTION - create a link that connects the Apollo Client to graphQL server
+// const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+// Instantiate ApolloClient
+// const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache() // store for state management
+// })
 
 ReactDOM.render(
   <BrowserRouter history={ browserHistory }>
-    <App />
+    //<ApolloProvider>
+      <App />
+    //</ApolloProvider>
   </BrowserRouter>, document.getElementById('app'));
