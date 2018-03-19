2f95805786d7551a764497ad77bdd16b61c6899a
Joe Ford <joefordiv7@gmail.com>
2018-03-19 15:38:37 -0400

    added semantic ui,css updated react,dom dependencies to work with semantic, added base navbar,categories,search

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -1,38 +1,18 @@
-import React from 'react';
-// import { withApollo } from 'react-apollo'; // for wrapping component with apollo client ?
-// import gql from 'graphql-tag'; // for constructing query
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Label } from 'semantic-ui-react'
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchQuery: '',
-    }
-  }
 
-  _executeSearch = async () => {
-    // TODO
-  }
 
-  render() {
-    return (
-      <div>
-        <div>
-          Search
-          <input type='text'
-            onChange={(e) => this.setState({ searchQuery: e.target.value })}
-          />
-          <button onClick={() => this._executeSearch()}>
-            SEARCH
-          </button>
-        </div>
-      </div>
-    )
-  }
+const resultRenderer = ({ title }) => <Label content={title} />
+
+resultRenderer.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
 }
 
-const LISTING_SEARCH_QUERY; // graphQL query
+const SearchBar = () => (
+  <SearchExampleStandard resultRenderer={resultRenderer} />
+)
 
-export default Search;
-
-// export default withApollo(Search); // for apollo
+export default SearchBar
