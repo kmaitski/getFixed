c37b01aeab9b1cfe29e5ba1fef270b47bb188a3b
gkolb <gkolb100@gmail.com>
2018-03-21 10:37:39 -0600

    Add find user branch

--- server/router/routes/users.js
+++ server/router/routes/users.js
@@ -6,6 +6,24 @@ module.exports = (app, db) => {
     })
   });
 
+  app.get('/user/:id', (req, res) => {
+    let user_id = req.params.id;
+    console.log(user_id)
+    db.users.findAll({
+      where: {
+        id: user_id
+      }
+    })
+    .then(user => {
+      res.status(200)
+      .send(user[0]);
+    })
+    .catch(err => {
+      res.status(400)
+      .send();
+    });
+  })
+
   app.post('/user', (req, res) => {
     let user = req.body
     console.log('user', user);
