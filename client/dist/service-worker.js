5ddc05dcd9faab8919b66fa2d0e5e4742e5b6591
kmaitski <kevin.maitski@gmail.com>
2018-03-24 11:33:40 -0700

    Add basic server worker
    All it does right now is console.log stuff in console

---
+++ client/dist/service-worker.js
@@ -0,0 +1,10 @@
+(function() {
+  self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+    self.skipWaiting();
+  });
+
+  self.addEventListener('activate', event => console.log('Service worker activating...'));
+
+  // self.addEventListener('fetch', event => console.log(event.request.url));
+})();
