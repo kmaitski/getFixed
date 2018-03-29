188f568c8e2db94ba0c5f96bcbb8e1c79986add5
kmaitski <kevin.maitski@gmail.com>
2018-03-29 15:28:56 -0700

    Saving to switch to master

--- client/src/components/problemsView.jsx
+++ client/src/components/problemsView.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import Problem from './problem.jsx';
 import { Card } from 'semantic-ui-react';
 
-const ProblemsView = ({problems}) => (
+const ProblemsView = (props) => (
+  <Query
+    query={query}
+    variables={props.category}
+  >
+    {({ loading, error, data }) => {
+      if (loading) { return <p>Loading...</p>; }
+      if (error) { return <p>Error :(</p>; }
+      return (
+        <div>
+          <Card.Group className="ui cards">
+            {problems.map((problem, index) => {
+              return <Problem
+                      key={index}
+                      problem={problem}
+                      userId={problem.user_id}
+                      index={index} 
+                    />
+            })}
+          </Card.Group>
+        </div>
+      )
+    }}
+  </Query>
 
-  <div>
-    <Card.Group className="ui cards" itemsPerRow={3}>
-      {problems.map((problem, index) => <Problem key={index} problem={problem} userId={problem.user_id} index={index} />)}
-    </Card.Group>
-  </div>
-
-)
+  )
 
 export default ProblemsView;
