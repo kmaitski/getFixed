c776943f8a2a9b06a0b3688b14d0840fc3ec5e29
Kevin Maitski <kevin.maitski@gmail.com>
2018-03-29 12:03:20 -0700

    Delete config.js

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  GCLOUD_PROJECT: "getfixed-198822",
-  DATA_BACKEND: "cloudsql",
-  MYSQL_USER: "root",
-  MYSQL_PASSWORD: "test",
-  INSTANCE_CONNECTION_NAME: "getfixed-198822:us-central1:get-fixed",
-  SQL_IP_ADDRESS: "35.193.181.113"
-};
