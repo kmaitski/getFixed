b1f8d93fa65ee0ac23270361f4d8d168891eca48
Joe Ford <joefordiv7@gmail.com>
2018-04-03 18:26:47 -0400

    updated login/sign up page and minor padding to grid cards

--- client/src/components/problem.jsx
+++ client/src/components/problem.jsx
@@ -4,15 +4,6 @@ import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
 import { Card, Icon, Image } from 'semantic-ui-react';
 import { Textfit } from 'react-textfit';
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-import './card.css';
->>>>>>> nav bar, categories fix button sticky design changes
-=======
-import './card.css';
->>>>>>> nav bar, categories fix button sticky design changes
 
 const colors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black'];
 
