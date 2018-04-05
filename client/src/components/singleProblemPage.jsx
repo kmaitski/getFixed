43a1ac0d452f041dbaff4025fa802041492488c4
Joe Ford <joefordiv7@gmail.com>
2018-04-05 16:47:25 -0400

    for rebase

--- client/src/components/singleProblemPage.jsx
+++ client/src/components/singleProblemPage.jsx
@@ -39,41 +39,40 @@ class SingleProblemPage extends React.Component {
           isLoggedIn={this.props.isLoggedIn}
           onLogout={this.props.onLogout}
         />
-      <div className="container"style={{position:'center'}}>
-        <Card>
-    <CardHeader
-      title={this.props.match.params.username}
+        <div className="container"style={{position:'center'}}>
+          <Card>
+            <CardHeader
+              title={this.props.match.params.username}
 
-      avatar="https://specials-images.forbesimg.com/imageserve/5a8d920d31358e4955adf197/416x416.jpg?background=000000&cropX1=755&cropX2=2357&cropY1=494&cropY2=2097"
-    />
-    <img
-        src={this.state.currentProblem.image}
-        style={{width:'60vw',height:'40vh'}}
-      />
-    <CardTitle title={this.state.currentProblem.description}subtitle={this.state.currentProblem.description} />
-    <CardText>
-      Share this Listing.
-    </CardText>
-    <CardActions>
-    <Button circular color='red' icon='empty heart' />
-    <Button circular color='pinterest' icon='pinterest' />
-    <Button circular color='facebook' icon='facebook' />
-    <Button circular color='twitter' icon='twitter' />
-    <Button circular color='instagram' icon='instagram' />
-    </CardActions>
-  </Card>
-        <div className="fixed-bottom" style={{ padding:" 0px 20px 20px 20px"}}>
-          <Link to ='/landing'>
-          <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect"
-          label="Filter"
-          >
-          <i className="material-icons">keyboard_return</i>
-          </button>
-          </Link>
+              avatar="https://specials-images.forbesimg.com/imageserve/5a8d920d31358e4955adf197/416x416.jpg?background=000000&cropX1=755&cropX2=2357&cropY1=494&cropY2=2097"
+            />
+            <img
+                src={this.state.currentProblem.image}
+                style={{width:'60vw',height:'40vh'}}
+              />
+            <CardTitle title={this.state.currentProblem.description}subtitle={this.state.currentProblem.description} />
+            <CardText>
+              Share this Listing.
+            </CardText>
+            <CardActions>
+            <Button circular color='red' icon='empty heart' />
+            <Button circular color='pinterest' icon='pinterest' />
+            <Button circular color='facebook' icon='facebook' />
+            <Button circular color='twitter' icon='twitter' />
+            <Button circular color='instagram' icon='instagram' />
+            </CardActions>
+          </Card>
+          <div className="fixed-bottom" style={{ padding:" 0px 20px 20px 20px"}}>
+            <Link to ='/landing'>
+              <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect"
+                label="Filter"
+              >
+              <i className="material-icons">keyboard_return</i>
+              </button>
+            </Link>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-
-      </div>
       </div>
     )
   }
