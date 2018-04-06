853e9b8e86583dbb7c2f2c489dcfaacc991b6cfe
kmaitski <kevin.maitski@gmail.com>
2018-04-06 07:41:41 -0700

    Move camera to bot and give users option to upload

--- client/src/components/CamShot3.jsx
+++ client/src/components/CamShot3.jsx
@@ -2,38 +2,51 @@ import React, { Component } from 'react';
 import Camera from 'react-camera';
 
 export default class CamShot3 extends React.Component {
-
- constructor(props) {
+  constructor(props) {
     super(props);
     this.takePicture = this.takePicture.bind(this);
-    this.state = {
-      cameraView: true
-    }
+    this.state = { cameraView: true };
   }
 
   takePicture() {
     this.camera.capture()
-    .then(blob => {
-      this.img.src = URL.createObjectURL(blob);
-      this.img.onload = () => { URL.revokeObjectURL(this.src); }
-      this.props.handlePhoto(this.img.src);
-    })
-    .then(
-      this.setState({cameraView: false})
-    )
+      .then((blob) => {
+        this.img.src = URL.createObjectURL(blob);
+        this.img.onload = () => { URL.revokeObjectURL(this.src); };
+        this.props.handlePhoto(this.img.src);
+      })
+      .then(this.setState({ cameraView: false }));
   }
 
   render() {
-    let view = this.state.cameraView
+    const style = {
+      preview: {
+        position: 'center',
+        height: '60%',
+        width: '40%',
+      },
+      captureContainer: {
+        top: '38%',
+        paddingLeft: '40%',
+      },
+      captureImage: {
+        position: 'center',
+        height: '60%',
+        width: '40%',
+      },
+    };
+    const view = this.state.cameraView
       ? <Camera
           style={style.preview}
           ref={(cam) => {
             this.camera = cam;
           }}
         >
-          <div className="fixed-top"style={style.captureContainer}>
-            <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"
-              onClick={this.takePicture}>
+          <div style={style.captureContainer}>
+            <button
+              className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored"
+              onClick={this.takePicture}
+            >
               <i className="material-icons">add</i>
             </button>
           </div>
@@ -53,21 +66,3 @@ export default class CamShot3 extends React.Component {
     );
   }
 }
-
-const style = {
-  preview: {
-    position: 'center',
-    height: '60%',
-    width: '40%'
-  },
-  captureContainer: {
-    top:'38%',
-    paddingLeft:'40%'
-  },
-
-  captureImage: {
-    position: 'center',
-    height: '60%',
-    width: '40%'
-  }
-};
